f31d3bca37f5da33478fb3abd8052f9128f2aea3
Tomás Ndlate <79701397+tomasndlate@users.noreply.github.com>
2025-05-11 20:43:28 +0100

    Time: 11 ms (82.98%) | Memory: 21.2 MB (43.50%) - LeetSync

---
+++ 49-group-anagrams/group-anagrams.py
@@ -0,0 +1,8 @@
+from collections import defaultdict
+class Solution:
+    def groupAnagrams(self, strs: List[str]) -> List[List[str]]:
+        anagrams = defaultdict(list)
+        for s in strs:
+            key = tuple(sorted(s))
+            anagrams[key].append(s)
+        return list(anagrams.values())
