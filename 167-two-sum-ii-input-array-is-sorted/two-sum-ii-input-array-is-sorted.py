67529d6bf27835fdf14d4f0808adfc7ec30e6a02
Tomás Ndlate <79701397+tomasndlate@users.noreply.github.com>
2025-05-13 08:03:26 +0100

    Time: 4 ms (48.83%) | Memory: 18.6 MB (60.01%) - LeetSync

--- 167-two-sum-ii-input-array-is-sorted/two-sum-ii-input-array-is-sorted.py
+++ 167-two-sum-ii-input-array-is-sorted/two-sum-ii-input-array-is-sorted.py
@@ -4,11 +4,8 @@ class Solution:
         i, j = 0, n-1
 
         while i < j:
-            while i + 1 < j and numbers[i] + numbers[j] < target:
-                i += 1
-            while i < j - 1 and numbers[i] + numbers[j] > target:
-                j -= 1
-            if numbers[i] + numbers[j] == target:
-                return [i+1, j+1]
+            while numbers[i] + numbers[j] < target: i += 1
+            while numbers[i] + numbers[j] > target: j -= 1
+            if numbers[i] + numbers[j] == target: return [i+1, j+1]
         
         return [-1, -1]
