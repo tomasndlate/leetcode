aaadb96e78ccfffc772501421f62e81da3bd9e34
Tomás Ndlate <79701397+tomasndlate@users.noreply.github.com>
2025-05-14 16:08:14 +0100

    Time: 131 ms (52.36%) | Memory: 18.2 MB (23.18%) - LeetSync

--- 424-longest-repeating-character-replacement/longest-repeating-character-replacement.py
+++ 424-longest-repeating-character-replacement/longest-repeating-character-replacement.py
@@ -2,24 +2,23 @@ from collections import defaultdict
 class Solution:
     def characterReplacement(self, s: str, k: int) -> int:
         repetitions = defaultdict(int)
-        value = None
+        max_freq = 0
         max_length, i = 0, 0
 
         for j in range(len(s)):
             
             # add to count
             repetitions[s[j]] += 1
-            if not value or repetitions[s[j]] > repetitions[value]:
-                value = s[j]
+            max_freq = max(max_freq, repetitions[s[j]])
+            #if repetitions[s[j]] > repetitions[value]:
+                #value = s[j]
 
             # while repetitions > k, remove i
-            while j - i + 1 - repetitions[value] > k:
+            while j - i + 1 - max_freq > k:
                 repetitions[s[i]] -= 1
-                if s[i] == value: #repetitions[s[i]] >= repetitions[value]:
-                    value = max(repetitions.items(), key=lambda x: x[1])[0]
-                    print(repetitions)
-                    print(max(repetitions.items()))
-                    print(value)
+                max_freq = max(max_freq, repetitions[s[i]])
+                #if s[i] == value:
+                    #value = max(repetitions.items(), key=lambda x: x[1])[0]
                 i += 1
             
             max_length = max(max_length, j - i + 1)
