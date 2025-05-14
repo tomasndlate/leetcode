a419ba4256e8d4d52a0c18565eae0633edac1ec2
Tomás Ndlate <79701397+tomasndlate@users.noreply.github.com>
2025-05-14 18:14:57 +0100

    Time: 27 ms (47.99%) | Memory: 17.9 MB (62.91%) - LeetSync

---
+++ 567-permutation-in-string/permutation-in-string.py
@@ -0,0 +1,22 @@
+from collections import Counter, defaultdict
+class Solution:
+    def checkInclusion(self, s1: str, s2: str) -> bool:
+        s1_count = defaultdict(int)
+        s2_count = defaultdict(int)
+        i = 0
+
+        for s in s1: s1_count[s] += 1
+
+        for s in s2:
+            s2_count[s] += 1
+            
+            # if count equals
+            if s1_count.items() == s2_count.items():
+                return True
+            
+            # else, move i until satisfy letter within count s1
+            while s2_count[s] > s1_count[s]:
+                s2_count[s2[i]] -= 1
+                i += 1
+        
+        return False
