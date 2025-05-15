8434f35bcb593c590c0d96e498e2b6cce2a37cf5
Tomás Ndlate <79701397+tomasndlate@users.noreply.github.com>
2025-05-15 09:52:55 +0100

    Time: 116 ms (69.82%) | Memory: 18.2 MB (23.22%) - LeetSync

--- 424-longest-repeating-character-replacement/longest-repeating-character-replacement.py
+++ 424-longest-repeating-character-replacement/longest-repeating-character-replacement.py
@@ -7,14 +7,16 @@ class Solution:
 
         for j, c in enumerate(s):
             count[c] += 1
-            maxf = max(maxf, count[c])
+            maxf = max(maxf, count[c]) # freq of most freq value
             
             if j - i + 1 - maxf <= k: # valid
                 res = max(res, j - i + 1)
             
-            while j - i + 1 - maxf > k:
+            while j - i + 1 - maxf > k: # invalid
                 count[s[i]] -= 1
                 i += 1
+                # maxf don't need to reduce - as to have 
+                # a higher res, maxf need to be this or higher
         
         return res
 
