df64499055cd6d385bee4f9a4c429c51ea7c29cd
Tomás Ndlate <79701397+tomasndlate@users.noreply.github.com>
2025-05-20 11:25:48 +0100

    Time: 0 ms (100.00%) | Memory: 18.1 MB (31.88%) - LeetSync

--- 153-find-minimum-in-rotated-sorted-array/find-minimum-in-rotated-sorted-array.py
+++ 153-find-minimum-in-rotated-sorted-array/find-minimum-in-rotated-sorted-array.py
@@ -4,14 +4,17 @@ class Solution:
         res = float('inf')
 
         while l <= r:
+            if nums[l] < nums[r]: # all section sorted
+                return min(res, nums[l])
+
             mid = (l + r) // 2
             res = min(res, nums[mid])
+
             if nums[l] > nums[mid]: # rotated on left
                 r = mid - 1
-            elif nums[r] < nums[mid]: # rotated on right
+            else: # rotated on right
                 l = mid + 1
-            else: # all sorted
-                return min(res, nums[l])
+                
         
         return res
         
