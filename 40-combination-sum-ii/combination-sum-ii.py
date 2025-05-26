81a8bdfc4da2a89ba12a1d0b1d87667e3b8d1dbf
Tomás Ndlate <79701397+tomasndlate@users.noreply.github.com>
2025-05-26 23:31:20 +0100

    Time: 27 ms (33.96%) | Memory: 18 MB (13.95%) - LeetSync

--- 40-combination-sum-ii/combination-sum-ii.py
+++ 40-combination-sum-ii/combination-sum-ii.py
@@ -3,17 +3,16 @@ class Solution:
         res = []
         candidates.sort()
 
-        def backtrack(start: int, path: List[int], total: int):
-            if total == target: res.append(path[:]); return
+        def dfs(start, path, total):
             if total > target: return
+            if total == target: res.append(path[:]); return
 
-            i = start
-            while i < len(candidates):
+            for i in range(start, len(candidates)):
+                if i > start and candidates[i-1] == candidates[i]:
+                    continue # skip equals at same level
                 path.append(candidates[i])
-                backtrack(i+1, path, total + candidates[i])
+                dfs(i+1, path[:], total + candidates[i])
                 path.pop()
-                i += 1
-                while i < len(candidates) and candidates[i-1] == candidates[i]: i += 1
 
-        backtrack(0, [], 0)
+        dfs(0, [], 0)
         return res
