69ffbe41e167f8bc9817afdcf73aedf45ffff325
Tomás Ndlate <79701397+tomasndlate@users.noreply.github.com>
2025-06-06 14:51:36 +0100

    Time: 6 ms (52.90%) | Memory: 19.3 MB (40.04%) - LeetSync

--- 207-course-schedule/course-schedule.py
+++ 207-course-schedule/course-schedule.py
@@ -1,26 +1,29 @@
 class Solution:
     def canFinish(self, numCourses: int, prerequisites: List[List[int]]) -> bool:
-        graph = collections.defaultdict(list)
-        for node, dependency in prerequisites:
-            graph[dependency].append(node)
-        
+        dependencies = {}
+
+        for course, pre in prerequisites:
+            if course not in dependencies:
+                dependencies[course] = []
+            dependencies[course].append(pre)
+
         visited = set()
         visiting = set()
+        def valid(course):
+            if course in visiting: return False
+            if course in visited: return True
+            
+            visiting.add(course)
 
-        def dfs(node): # return true if found cycle
-            if node in visiting: return True # cycle
-            if (   node in visited
-                or node not in graph):
-                return False
+            for pre in dependencies.get(course, []):
+                if not valid(pre):
+                    return False
+            
+            visiting.remove(course)
+            visited.add(course)
+            return True
+        
+        for course in range(numCourses):
+            if not valid(course): return False
 
-            visited.add(node)
-            visiting.add(node)
-            for neighbor in graph[node]:
-                if dfs(neighbor): return True
-            visiting.remove(node)
-            return False
-
-        for node in graph:
-            if dfs(node): # has cycle - cannot finish course
-                return False
         return True
