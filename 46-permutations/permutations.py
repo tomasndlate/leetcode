6c8806c80f5e676d6ac8c0d3bd194266bea0a484
Tomás Ndlate <79701397+tomasndlate@users.noreply.github.com>
2025-06-06 14:30:21 +0100

    Time: 0 ms (100.00%) | Memory: 17.7 MB (95.37%) - LeetSync

--- 46-permutations/permutations.py
+++ 46-permutations/permutations.py
@@ -1,15 +1,20 @@
 class Solution:
     def permute(self, nums: List[int]) -> List[List[int]]:
+        # [] -> [1] -> [1,2] -> [1,2,3]
+        #                    -> [1,3,2]
+        #                    -> [3,1,2]
+        #           -> [2,1] -> [2,1,3]
+        #                    -> [2,3,1]
+        #                    -> [3,2,1]
         permutations = [[]]
 
         for n in nums:
-            new_perm = []
-            for subset in permutations:
-                for i in range(len(subset) + 1):
-                    subset_copy = subset.copy()
-                    subset_copy.insert(i, n)
-                    new_perm.append(subset_copy)
-            permutations = new_perm
+            newPerms = []
+            for perm in permutations:
+                for i in range(len(perm) + 1):
+                    permCopy = perm[:]
+                    permCopy.insert(i, n)
+                    newPerms.append(permCopy)
+            permutations = newPerms
         
         return permutations
-        
