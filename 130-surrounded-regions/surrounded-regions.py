198d04571deae1bec5f4ebaf575934a39a352e6f
Tomás Ndlate <79701397+tomasndlate@users.noreply.github.com>
2025-06-09 08:09:00 +0100

    Time: 7 ms (66.54%) | Memory: 21.8 MB (67.12%) - LeetSync

--- 130-surrounded-regions/surrounded-regions.py
+++ 130-surrounded-regions/surrounded-regions.py
@@ -3,36 +3,43 @@ class Solution:
         """
         Do not return anything, modify board in-place instead.
         """
+        # CHECK NOT SURROUNDED - CHANGE TO TEMPORARY VALUE
+        # THEN REPLACE ALL VALUES
+
         rows, cols = len(board), len(board[0])
-        visited = [ [False] * cols for _ in range(rows) ]
-
-        def dfs(row, col) -> bool:
-            if (   row < 0 or row >= rows
-                or col < 0 or col >= cols
-                or board[row][col] == "X"
-                or visited[row][col]
-                ): return
-            
-            visited[row][col] = True
-
-            dfs(row - 1, col)
-            dfs(row + 1, col)
-            dfs(row, col - 1)
-            dfs(row, col + 1) 
-
+        directions = [(-1, 0), (1, 0), (0, -1), (0, 1)]
+        queue = collections.deque()
         
-        # traverse horizontal borders top/bottom - visit all "O"
+        # LOOP THROUGH BORDERS
+        ## horizontal
         for c in range(cols):
-            if board[0][c] == "O" and not visited[0][c]: dfs(0, c)
-            if board[rows-1][c] == "O" and not visited[rows-1][c]: dfs(rows-1, c)
-
-        # traverse vertical borders left/right - visit all "O"
+            if board[0][c] == "O":      queue.append((0, c))
+            if board[rows-1][c] == "O": queue.append((rows-1, c))
+        ## vertical
         for r in range(rows):
-            if board[r][0] == "O" and not visited[r][0]: dfs(r, 0)
-            if board[r][cols-1] == "O" and not visited[r][cols-1]: dfs(r, cols-1)
+            if board[r][0] == "O":      queue.append((r, 0))
+            if board[r][cols-1] == "O": queue.append((r, cols-1))
 
-        # every "O" not visited change to "X"
+        while queue:
+            row, col = queue.popleft()
+            if board[row][col] == "V": continue
+
+            board[row][col] = "V"
+
+            for rdir, cdir in directions:
+                r, c = row + rdir, col + cdir
+                if (   r < 0 or r >= rows 
+                    or c < 0 or c >= cols):
+                    continue # out of bounds
+                if board[r][c] == "O":
+                    queue.append((r, c))
+
+        # LOOP AND CHANGE (V->O), and (O->X)
         for r in range(rows):
             for c in range(cols):
-                if not visited[r][c] and board[r][c] == "O":
+                if board[r][c] == "O": # surrounded
                     board[r][c] = "X"
+                if board[r][c] == "V": # not surrounded
+                    board[r][c] = "O"
+        
+        return board
