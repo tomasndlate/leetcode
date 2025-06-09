b9adbbc37f8512c321221a368842435793711af0
Tomás Ndlate <79701397+tomasndlate@users.noreply.github.com>
2025-06-09 08:40:57 +0100

    Time: 1028 ms (17.10%) | Memory: 20.8 MB (37.30%) - LeetSync

--- 15-3sum/3sum.py
+++ 15-3sum/3sum.py
@@ -1,20 +1,33 @@
 class Solution:
     def threeSum(self, nums: List[int]) -> List[List[int]]:
+        # TWO POINTERS
+        nums.sort()
+        print(nums)
         n = len(nums)
         res = []
-        nums.sort()
 
-        for i in range(n - 2):
-            if i > 0 and nums[i] == nums[i-1]: continue
+        for i in range(n-2):
+            if i > 0 and nums[i] == nums[i-1]:
+                continue
+
             j, k = i + 1, n - 1
+
             while j < k:
-                threeSum = nums[i] + nums[j] + nums[k]
-                if threeSum < 0: j += 1
-                if threeSum > 0: k -= 1
-                if threeSum == 0: 
+                if nums[i] + nums[j] + nums[k] < 0:
+                    j += 1
+                    while j < k and nums[j] == nums[j-1]:
+                        j += 1
+                elif nums[i] + nums[j] + nums[k] > 0:
+                    k -= 1
+                    while j < k and nums[k] == nums[k+1]:
+                        k -= 1
+                else:
                     res.append([nums[i], nums[j], nums[k]])
                     j += 1
-                    while nums[j] == nums[j-1] and j < k: j+= 1
+                    while j < k and nums[j] == nums[j-1]:
+                        j += 1
+                    k -= 1
+                    while j < k and nums[k] == nums[k+1]:
+                        k -= 1
 
         return res
-        
