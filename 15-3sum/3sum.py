7048532d94be6f3bd055305f9c95d7db83859a59
Tomás Ndlate <79701397+tomasndlate@users.noreply.github.com>
2025-06-09 08:43:07 +0100

    Time: 731 ms (29.10%) | Memory: 20.7 MB (37.30%) - LeetSync

--- 15-3sum/3sum.py
+++ 15-3sum/3sum.py
@@ -15,19 +15,15 @@ class Solution:
             while j < k:
                 if nums[i] + nums[j] + nums[k] < 0:
                     j += 1
-                    while j < k and nums[j] == nums[j-1]:
-                        j += 1
                 elif nums[i] + nums[j] + nums[k] > 0:
                     k -= 1
-                    while j < k and nums[k] == nums[k+1]:
-                        k -= 1
                 else:
                     res.append([nums[i], nums[j], nums[k]])
                     j += 1
                     while j < k and nums[j] == nums[j-1]:
                         j += 1
-                    k -= 1
-                    while j < k and nums[k] == nums[k+1]:
-                        k -= 1
+                    #k -= 1
+                    #while j < k and nums[k] == nums[k+1]:
+                        #k -= 1
 
         return res
