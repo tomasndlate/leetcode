bff189159c4d33ccd61f88c2bbb75aca1aa1608d
Tomás Ndlate <79701397+tomasndlate@users.noreply.github.com>
2025-06-10 07:56:48 +0100

    Time: 7 ms (21.44%) | Memory: 20 MB (11.99%) - LeetSync

--- 747-min-cost-climbing-stairs/min-cost-climbing-stairs.py
+++ 747-min-cost-climbing-stairs/min-cost-climbing-stairs.py
@@ -1,18 +1,21 @@
 class Solution:
     def minCostClimbingStairs(self, cost: List[int]) -> int:
-        # DP BOTTOM-UP
-        one, two = cost[-1], 0
-        for i in range(len(cost) - 2, -1, -1):
-            one, two = cost[i] + min(one, two), one
-        return min(one, two)
+        # TOP-DOWN: RECURSION + MEMOIZATION
+        cache = {}
 
-        ## MEMOIZATION
-        #cache = {}
-        #def dfs(i):
-            #if i >= len(cost): return 0
-            #if i in cache: return cache[i]
+        def dfs(i):
+            if i >= len(cost):
+                return 0
+            
+            if i in cache:
+                return cache[i]
+            
+            cache[i] = cost[i] + min(dfs(i+1), dfs(i+2))
 
-            #cache[i] = cost[i] + min(dfs(i+1), dfs(i+2))
-            #return cache[i]
+            return cache[i]
+        
+        return min(dfs(0), dfs(1))
 
-        #return min(dfs(0), dfs(1))
+        # BOTTOM-UP: TABULATION
+            
+        
