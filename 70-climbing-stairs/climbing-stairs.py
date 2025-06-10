a165952d60abbb459c7b2504485fb256d8a3972c
Tomás Ndlate <79701397+tomasndlate@users.noreply.github.com>
2025-06-10 07:40:04 +0100

    Time: 0 ms (100.00%) | Memory: 17.9 MB (7.87%) - LeetSync

--- 70-climbing-stairs/climbing-stairs.py
+++ 70-climbing-stairs/climbing-stairs.py
@@ -1,17 +1,19 @@
 class Solution:
     def climbStairs(self, n: int) -> int:
-        #  0,    1,    2,    3,    4,    5
-        # [                      one=1 two=1]
-        # DP BOTTOM-UP
-        one, two = 1, 1
-        for num in range(1, n):
-            one, two = one + two, one
-        return one
-        ## MEMOIZATION SOLUTION
-        #cache = {1: 1, 2: 2}
-        #def dfs(num):
-            #if num in cache: return cache[num]
-            ## position after -> 1 step + 2 steps
-            #cache[num] = dfs(num-1) + dfs(num-2) 
-            #return cache[num]
-        #return dfs(n)
+        # TOP-DOWN: RECURSION + MEMOIZATION
+        cache = {}
+        def dfs(i):
+            if i > n: return 0
+            if i == n: return 1
+            
+            if i in cache:
+                return cache[i]
+
+            cache[i] = dfs(i+1) + dfs(i+2)
+
+            return cache[i]
+        
+        return dfs(1) + dfs(2)
+
+        # BOTTOM-UP: TABULATION
+        
