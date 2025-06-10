e9dc48df22a00b353316fc409e0f5c384cd2996e
Tomás Ndlate <79701397+tomasndlate@users.noreply.github.com>
2025-06-10 08:03:50 +0100

    Time: 3 ms (7.94%) | Memory: 17.8 MB (34.69%) - LeetSync

--- 198-house-robber/house-robber.py
+++ 198-house-robber/house-robber.py
@@ -1,26 +1,19 @@
 class Solution:
     def rob(self, nums: List[int]) -> int:
-        # DP BOTTOM-UP - Tabulation
-        n = len(nums)
-        if n == 1: return nums[0]
+        # TOP-DOWN: RECURSION + MEMOIZATION
+        cache = {}
 
-        for i in range(n-2, -1, -1):
-            first =  nums[i+2] if i+2 < n else 0
-            second = nums[i+3] if i+3 < n else 0
-
-            nums[i] += max(first, second)
-
-        return max(nums[0], nums[1])
-
-        # MEMOIZATION - Recursion + Cache        
-        #cache = {}
-
-        #def dfs(i):
-            #if i >= len(nums): return 0
-            #if i in cache: return cache[i]
+        def dfs(i):
+            if i >= len(nums):
+                return 0
             
-            #cache[i] = nums[i] + max(dfs(i+2), dfs(i+3))
-            #return cache[i]
+            if i in cache:
+                return cache[i]
 
-        #return max(dfs(0), dfs(1))
+            cache[i] = nums[i] + max(dfs(i+2), dfs(i+3))
+
+            return cache[i]
+        
+        return max(dfs(0), dfs(1))
+        # BOTTOM-UP: TABULATION
         
