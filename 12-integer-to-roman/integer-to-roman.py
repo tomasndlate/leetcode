4769c31e25a19a13f5fcc321cb1f3a35e768af75
Tomás Ndlate <79701397+tomasndlate@users.noreply.github.com>
2025-06-14 13:52:44 +0100

    Time: 0 ms (100.00%) | Memory: 18 MB (5.84%) - LeetSync

--- 12-integer-to-roman/integer-to-roman.py
+++ 12-integer-to-roman/integer-to-roman.py
@@ -5,10 +5,10 @@ class Solution:
             ("XL", 40), ("L", 50), ("XC", 90), ("C", 100),
             ("CD", 400), ("D", 500), ("CM", 900), ("M", 1000)
             ]
-        res = ""
+        res = []
         
         for symbol, value in reversed(symbols):
-            res += symbol * (num // value)
+            res.append(symbol * (num // value))
             num = num % value
         
-        return res
+        return "".join(res)
