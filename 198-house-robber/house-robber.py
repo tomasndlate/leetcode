60ad11361005574f1f43c0dfda2152a59c5dc9be
Tomás Ndlate <79701397+tomasndlate@users.noreply.github.com>
2025-06-15 19:11:18 +0100

    Time: 0 ms (100.00%) | Memory: 17.8 MB (34.67%) - LeetSync

--- 198-house-robber/house-robber.py
+++ 198-house-robber/house-robber.py
@@ -1,19 +1,18 @@
 class Solution:
     def rob(self, nums: List[int]) -> int:
-        # TOP-DOWN: RECURSION + MEMOIZATION
+        # TOP-DOWN
         cache = {}
-
         def dfs(i):
-            if i >= len(nums):
-                return 0
-            
             if i in cache:
                 return cache[i]
+            if i >= len(nums):
+                return 0 # total stole
+            
+            res1 = dfs(i+2) # 1 of interval
+            res2 = dfs(i+3) # 2 of interval
 
-            cache[i] = nums[i] + max(dfs(i+2), dfs(i+3))
-
+            cache[i] = nums[i] + max(res1, res2)
             return cache[i]
         
         return max(dfs(0), dfs(1))
-        # BOTTOM-UP: TABULATION
-        
+        # BOTTOM-UP
