fe3bf397c43afc0edd70c034247d56a7338a186e
Tomás Ndlate <79701397+tomasndlate@users.noreply.github.com>
2025-06-15 20:16:23 +0100

    Time: 2 ms (12.60%) | Memory: 17.8 MB (38.84%) - LeetSync

--- 213-house-robber-ii/house-robber-ii.py
+++ 213-house-robber-ii/house-robber-ii.py
@@ -1,27 +1,22 @@
 class Solution:
     def rob(self, nums: List[int]) -> int:
-        # TOP-DOWN: RECURSION + MEMOIZATION
-        if len(nums) == 1:
+        n = len(nums)
+        if n == 1:
             return nums[0]
-
-        return max(self.helper(nums[:-1]), self.helper(nums[1:]))
-        # BOTTOM-UP: TABULATION
-    
-    def helper(self, nums):
+            
         cache = {}
-
-        def dfs(i):
-            if i >= len(nums):
+        def dfs(i, bound):
+            key = (i, bound)
+            if key in cache:
+                return cache[key]
+            if i >= bound:
                 return 0
-            
-            if i in cache:
-                return cache[i]
-            
-            cache[i] = nums[i] + max(dfs(i+2), dfs(i+3))
 
-            return cache[i]
-        
-        return max(dfs(0), dfs(1))
+            robRes = nums[i] + dfs(i+2, bound)
+            skipRes = dfs(i+1, bound)
+
+            cache[key] = max(robRes, skipRes)
+            return cache[key]
 
         
-        
+        return max(dfs(0, n-1), dfs(1, n))
