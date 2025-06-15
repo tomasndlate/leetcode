f31702e937ac78931042429af10671c538bd26bd
Tomás Ndlate <79701397+tomasndlate@users.noreply.github.com>
2025-06-15 20:52:11 +0100

    Time: 238 ms (76.50%) | Memory: 20.1 MB (82.60%) - LeetSync

--- 200-number-of-islands/number-of-islands.py
+++ 200-number-of-islands/number-of-islands.py
@@ -4,29 +4,24 @@ class Solution:
         directions = [(-1, 0), (1, 0), (0, -1), (0, 1)]
 
         def dfs(row, col):
-            if grid[row][col] == "0": # already visited 
+            if grid[row][col] == "0":
                 return
             
-            grid[row][col] = "0" # mark as visited
+            grid[row][col] = "0"
 
             for x, y in directions:
-                r = row + x
+                r = row + x 
                 c = col + y
-                if (   r < 0 or r >= rows
-                    or c < 0 or c >= cols): # out of bounds
-                    continue
-                if grid[r][c] == "1":
+                if (    r >= 0 and r < rows
+                    and c >= 0 and c < cols
+                    and grid[r][c] == "1"):
                     dfs(r, c)
         
         islands = 0
         for r in range(rows):
             for c in range(cols):
                 if grid[r][c] == "1":
-                    dfs(r, c)
                     islands += 1
-
-        return islands
-
-
-            
+                    dfs(r, c)
         
+        return islands
