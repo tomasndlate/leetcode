fc186e28931391c80f6b280c9f4cf3857f30262c
Tomás Ndlate <79701397+tomasndlate@users.noreply.github.com>
2025-06-15 17:23:26 +0100

    Time: 60 ms (85.10%) | Memory: 18.2 MB (54.50%) - LeetSync

--- 76-minimum-window-substring/minimum-window-substring.py
+++ 76-minimum-window-substring/minimum-window-substring.py
@@ -1,35 +1,37 @@
-from collections import Counter
 class Solution:
     def minWindow(self, s: str, t: str) -> str:
-        # SLIDING WINDOW
-        if not s or not t: return ""
-        window, t_count = {}, {}
+        # hashmap of t count
+        tCount = {}
         for c in t:
-            t_count[c] = 1 + t_count.get(c, 0)
+            tCount[c] = tCount.get(c, 0) + 1
         
-        have, need = 0, len(t_count)
-        res, resLen = (None, None), float('inf')
+        # window properties
+        window = {}
+        windowLen = float('inf') # easily assign first window
+        left = right = None # response minimum window
+        have, need = 0, len(tCount) # O(1) t within s window
 
-        left = 0
-        for right in range(len(s)):
-            c = s[right]
-            window[c] = 1 + window.get(c, 0)
-            if c in t_count and window[c] == t_count[c]:
+        start = 0
+        for end in range(len(s)):
+            c = s[end]
+            window[c] = window.get(c, 0) + 1
+            # char in t and first time reaching update 'have'
+            if c in tCount and window[c] == tCount[c]:
                 have += 1
-            
+            # window include t, update min window
             while have == need:
-                # compare minimum window
-                if right - left + 1 < resLen:
-                    res = (left, right)
-                    resLen = right - left + 1
-
-                # shrink window
-                window[s[left]] -= 1
-                if s[left] in t_count and window[s[left]] < t_count[s[left]]:
+                # smaller window
+                if end - start + 1 < windowLen:
+                    windowLen = end - start + 1
+                    left = start
+                    right = end
+                # remove start
+                window[s[start]] -= 1
+                if s[start] in tCount and window[s[start]] < tCount[s[start]]:
                     have -= 1
-                left += 1
-            
-        return s[res[0]:res[1]+1] if resLen != float('inf') else ""
+                start += 1
+        
+        return s[left:right+1] if windowLen != float('inf') else ""
+        
 
         
-                 
