5e088536197f8e5b35d452147eb2fcf38800ee44
Tomás Ndlate <79701397+tomasndlate@users.noreply.github.com>
2025-06-15 22:53:42 +0100

    Time: 44 ms (50.27%) | Memory: 18.2 MB (56.93%) - LeetSync

--- 133-clone-graph/clone-graph.py
+++ 133-clone-graph/clone-graph.py
@@ -12,17 +12,17 @@ class Solution:
         if not node:
             return None
         clones = {}
-        queue = collections.deque([node])
 
+        queue = collections.deque([node])
         while queue:
             cur = queue.popleft()
             if cur not in clones:
                 clones[cur] = Node(cur.val)
-
+            
             for neigh in cur.neighbors:
                 if neigh not in clones:
                     clones[neigh] = Node(neigh.val)
                     queue.append(neigh)
                 clones[cur].neighbors.append(clones[neigh])
-        
+            
         return clones[node]
