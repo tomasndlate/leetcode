ef0fd70042f9513d0b118af002da307f55603e2d
Tomás Ndlate <79701397+tomasndlate@users.noreply.github.com>
2025-06-15 22:17:31 +0100

    Time: 4 ms (42.57%) | Memory: 18 MB (44.54%) - LeetSync

--- 139-word-break/word-break.py
+++ 139-word-break/word-break.py
@@ -1,19 +1,20 @@
 class Solution:
     def wordBreak(self, s: str, wordDict: List[str]) -> bool:
         words = set(wordDict)
-
         cache = {}
+
         def dfs(i):
             if i in cache:
                 return cache[i]
             if i >= len(s):
                 return True
 
-            completed = False
             for j in range(i, len(s)):
                 if s[i:j+1] in words and dfs(j+1): # subword found
-                    completed = True
-            cache[i] = completed
-            return completed
+                    cache[i] = True
+                    return True
+
+            cache[i] = False
+            return False
         
         return dfs(0)
