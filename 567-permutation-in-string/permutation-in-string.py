10b31e0cf6a535769392ab65d0f592c7db1e27b3
Tomás Ndlate <79701397+tomasndlate@users.noreply.github.com>
2025-06-16 09:53:10 +0100

    Time: 23 ms (54.18%) | Memory: 17.8 MB (62.18%) - LeetSync

--- 567-permutation-in-string/permutation-in-string.py
+++ 567-permutation-in-string/permutation-in-string.py
@@ -1,22 +1,28 @@
-from collections import defaultdict
 class Solution:
     def checkInclusion(self, s1: str, s2: str) -> bool:
-        s1_count = defaultdict(int)
-        s2_count = defaultdict(int)
-        i = 0
-
-        for s in s1: s1_count[s] += 1
-
-        for s in s2:
-            s2_count[s] += 1
-            
-            # if count equals
-            if s1_count.items() == s2_count.items():
-                return True
-            
-            # else, move i until satisfy letter within count s1
-            while s2_count[s] > s1_count[s]:
-                s2_count[s2[i]] -= 1
-                i += 1
+        permMap = {}
+        for s in s1:
+            permMap[s] = permMap.get(s, 0) + 1
         
+        window = {}
+        have, need = 0, len(permMap)
+
+        left = 0
+        for right in range(len(s2)):
+            window[s2[right]] = window.get(s2[right], 0) + 1
+            if s2[right] in permMap and window[s2[right]] == permMap[s2[right]]:
+                have += 1
+            
+            while have == need:
+                if right - left + 1 == len(s1):
+                    return True
+
+                # shrink
+                window[s2[left]] -= 1
+                if s2[left] in permMap and window[s2[left]] < permMap[s2[left]]:
+                    have -= 1
+                left += 1
+            
         return False
+
+        
