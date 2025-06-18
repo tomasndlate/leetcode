ffacd086c2bc3a13a08174051a3d12bc18525e8c
Tomás Ndlate <79701397+tomasndlate@users.noreply.github.com>
2025-06-18 20:05:07 +0100

    Time: 0 ms (100.00%) | Memory: 18 MB (24.08%) - LeetSync

--- 392-is-subsequence/is-subsequence.py
+++ 392-is-subsequence/is-subsequence.py
@@ -1,14 +1,12 @@
 class Solution:
     def isSubsequence(self, s: str, t: str) -> bool:
-        if not s:
-            return True
-            
+        if not s: return True
+
         i = 0
         for letter in t:
             if letter == s[i]:
                 i += 1
-            
-            if i == len(s):
-                return True
+                if i == len(s):
+                    return True
         
         return False
