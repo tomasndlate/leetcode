f611f850e1e246fd7ec9ebdc4b4fa1e3fe014d00
Tomás Ndlate <79701397+tomasndlate@users.noreply.github.com>
2025-06-18 20:21:24 +0100

    Time: 80 ms (66.49%) | Memory: 27.5 MB (39.25%) - LeetSync

--- 643-maximum-average-subarray-i/maximum-average-subarray-i.py
+++ 643-maximum-average-subarray-i/maximum-average-subarray-i.py
@@ -3,12 +3,12 @@ class Solution:
         if not nums:
             return 0.0
 
-        left = 0
-        windowSum = sum(nums[left:k])
+        windowSum = sum(nums[:k])
         windowMax = windowSum
+        left = 0
 
         for right in range(k, len(nums)):
-            windowSum += -nums[left] + nums[right]
+            windowSum = windowSum - nums[left] + nums[right]
             left += 1
 
             windowMax = max(windowMax, windowSum)
