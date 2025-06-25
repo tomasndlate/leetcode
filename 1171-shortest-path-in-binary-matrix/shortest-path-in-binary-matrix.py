ca52c9a9f437a871be0c6c0875798a33aa32d0d3
Tomás Ndlate <79701397+tomasndlate@users.noreply.github.com>
2025-06-25 23:18:35 +0100

    Time: 199 ms (57.68%) | Memory: 19.7 MB (18.50%) - LeetSync

--- 1171-shortest-path-in-binary-matrix/shortest-path-in-binary-matrix.py
+++ 1171-shortest-path-in-binary-matrix/shortest-path-in-binary-matrix.py
@@ -6,26 +6,23 @@ class Solution:
         rows, cols = len(grid), len(grid[0])
         directions = [(-1, 0), (-1, 1), (0, 1), (1, 1), (1, 0), (1, -1), (0, -1), (-1, -1)]
         visited = set()
+        visited.add((0, 0))
 
-        queue = collections.deque([(0,0,0)])
+        queue = collections.deque([(0,0,1)])
         while queue:
             row, col, path = queue.popleft()
-            
-            if (row, col) in visited:
-                continue
+
             if (row, col) == (rows-1, cols-1): # final
-                return path + 1
-            
-            visited.add((row, col))
+                return path
             
             for x, y in directions:
                 r = row + x
                 c = col + y
-                
                 if (    r >= 0 and r < rows
                     and c >= 0 and c < cols
                     and (r, c) not in visited
                     and grid[r][c] == 0):
+                    visited.add((r, c))
                     queue.append((r, c, path + 1))
         
         return -1
