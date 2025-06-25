909cd57e2c387d4cdcf84b4e968297ab3dfd9cfd
Tomás Ndlate <79701397+tomasndlate@users.noreply.github.com>
2025-06-25 22:41:27 +0100

    Time: 305 ms (60.09%) | Memory: 18 MB (44.91%) - LeetSync

--- 5-longest-palindromic-substring/longest-palindromic-substring.py
+++ 5-longest-palindromic-substring/longest-palindromic-substring.py
@@ -1,35 +1,24 @@
 class Solution:
     def longestPalindrome(self, s: str) -> str:
         n = len(s)
-        if n == 1:
-            return s
-        
-        max_palindrome = ""
+        palindrome = ""
 
-        def palindrome(l, r):
-            if s[l] != s[r]:
-                return r, r
+        for i in range(n):
 
-            while l - 1 >= 0 and r + 1 < n and s[l-1] == s[r+1]:
+            # odd
+            l = r = i
+            while l >= 0 and r < n and s[l] == s[r]:
+                if r - l + 1 > len(palindrome):
+                    palindrome = s[l: r+1]
                 l -= 1
                 r += 1
-            
-            return l, r
 
-        for i in range(1, n):
-            # odd
-            l, r = palindrome(i, i)
-            if r - l + 1 >= len(max_palindrome):
-                max_palindrome = s[l:r+1]
-            print("odd", l, r)
             # even
-            l, r = palindrome(i - 1, i)
-            if r - l + 1 >= len(max_palindrome):
-                max_palindrome = s[l:r+1]
-            print("even", l, r)
+            l, r = i, i + 1
+            while l >= 0 and r < n and s[l] == s[r]:
+                if r - l + 1 > len(palindrome):
+                    palindrome = s[l: r+1]
+                l -= 1
+                r += 1
         
-        return max_palindrome
-
-        # TOP-DOWN: RECURSION + MEMOIZATION
-        # BOTTOM-UP: TABULTION
-        
+        return palindrome
