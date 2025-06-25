6efb9fa0949c909b272d0a5f3c5e563dc5b5c8e2
Tomás Ndlate <79701397+tomasndlate@users.noreply.github.com>
2025-06-25 19:35:39 +0100

    Time: 0 ms (100.00%) | Memory: 18.2 MB (44.52%) - LeetSync

--- 74-search-a-2d-matrix/search-a-2d-matrix.py
+++ 74-search-a-2d-matrix/search-a-2d-matrix.py
@@ -2,19 +2,20 @@ class Solution:
     def searchMatrix(self, matrix: List[List[int]], target: int) -> bool:
         row_min = 0
         row_max = len(matrix) - 1
-        col_min = 0
-        col_max = len(matrix[0]) - 1
 
         while row_min <= row_max:
             row_mid = (row_min + row_max) // 2
             
-            if target < matrix[row_mid][col_min]:
+            if target < matrix[row_mid][0]:
                 row_max = row_mid - 1
 
-            elif target > matrix[row_mid][col_max]:
+            elif target > matrix[row_mid][-1]:
                 row_min = row_mid + 1
                 
             else:
+                col_min = 0
+                col_max = len(matrix[0]) - 1
+
                 while col_min <= col_max:
                     col_mid = (col_min + col_max) // 2
                     
@@ -26,6 +27,7 @@ class Solution:
 
                     else:
                         return True
-        
+                return False
+
         return False
             
