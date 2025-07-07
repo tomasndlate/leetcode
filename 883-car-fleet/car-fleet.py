343ca711d0a8737173e5f8b6e590b12fce86181f
Tomás Ndlate <79701397+tomasndlate@users.noreply.github.com>
2025-07-07 09:42:11 +0100

    Time: 171 ms (48.60%) | Memory: 40.7 MB (19.38%) - LeetSync

--- 883-car-fleet/car-fleet.py
+++ 883-car-fleet/car-fleet.py
@@ -1,15 +1,20 @@
 class Solution:
     def carFleet(self, target: int, position: List[int], speed: List[int]) -> int:
+        if not position or not speed:
+            return 0
+
         # target = (speed * time) + position <=> time = (target - position) / speed
         cars = [ (p, (target - p) / s) for p, s in zip(position, speed) ] # (position, time)
         cars.sort(key=lambda x: x[0], reverse=True)
         
-        time = 1
-        fleets = [cars[0][time]] # initiate with first car time to reach
+        TIME_IDX = 1
+        fleets = 1
+        fleet_time = cars[0][TIME_IDX] # initiate with first car time to reach
 
         for car in cars[1:]:
-            # if time to reach higher than fleet, new fleet, else nothing happen
-            if car[time] > fleets[-1]:
-                fleets.append(car[time])
+            # if time to reach higher than fleet, new fleet
+            if car[TIME_IDX] > fleet_time:
+                fleet_time = car[TIME_IDX]
+                fleets += 1
         
-        return len(fleets)
+        return fleets
