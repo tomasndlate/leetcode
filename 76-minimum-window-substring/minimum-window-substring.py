7cf0d8ba2ee7d9006dbdf1816293ff45099f3919
Tomás Ndlate <79701397+tomasndlate@users.noreply.github.com>
2025-07-16 08:27:21 +0100

    Time: 75 ms (57.34%) | Memory: 18.4 MB (16.28%) - LeetSync

--- 76-minimum-window-substring/minimum-window-substring.py
+++ 76-minimum-window-substring/minimum-window-substring.py
@@ -1,37 +1,31 @@
+from collections import Counter, defaultdict
 class Solution:
     def minWindow(self, s: str, t: str) -> str:
-        # hashmap of t count
-        tCount = {}
-        for c in t:
-            tCount[c] = tCount.get(c, 0) + 1
-        
-        # window properties
-        window = {}
-        windowLen = float('inf') # easily assign first window
-        left = right = None # response minimum window
-        have, need = 0, len(tCount) # O(1) t within s window
+        windowReq = Counter(t)
+        window = defaultdict(int)
+        minLength = float('inf')
+        start = end = None
+        curReq, needReq = 0, len(windowReq) # how many requisites need to match
 
-        start = 0
-        for end in range(len(s)):
-            c = s[end]
-            window[c] = window.get(c, 0) + 1
-            # char in t and first time reaching update 'have'
-            if c in tCount and window[c] == tCount[c]:
-                have += 1
-            # window include t, update min window
-            while have == need:
-                # smaller window
-                if end - start + 1 < windowLen:
-                    windowLen = end - start + 1
-                    left = start
-                    right = end
-                # remove start
-                window[s[start]] -= 1
-                if s[start] in tCount and window[s[start]] < tCount[s[start]]:
-                    have -= 1
-                start += 1
-        
-        return s[left:right+1] if windowLen != float('inf') else ""
-        
+        left = 0
+        for right, c in enumerate(s):
 
+            window[s[right]] += 1
+
+            # check if requisites match (exact match)
+            if s[right] in windowReq and window[s[right]] == windowReq[s[right]]:
+                curReq += 1
+                
+                # if curReq match needReq window is valid
+                while curReq == needReq:
+                    # update window if new min
+                    if right - left + 1 < minLength:
+                        minLength = right - left + 1
+                        start, end = left, right
+                    # shrink window
+                    window[s[left]] -= 1
+                    if s[left] in windowReq and window[s[left]] < windowReq[s[left]]:
+                        curReq -= 1
+                    left += 1
         
+        return s[start:end+1] if start != None else ""
