936c02802266f4950619aed4c0faecfd2fd1cce5
Tomás Ndlate <79701397+tomasndlate@users.noreply.github.com>
2025-07-17 12:47:48 +0100

    Time: 167 ms (37.11%) | Memory: 19.1 MB (9.88%) - LeetSync

--- 907-koko-eating-bananas/koko-eating-bananas.py
+++ 907-koko-eating-bananas/koko-eating-bananas.py
@@ -1,24 +1,22 @@
 class Solution:
     def minEatingSpeed(self, piles: List[int], h: int) -> int:
-        minK, maxK = 1, max(piles)
+        minK = 1 # min velocity
+        maxK = max(piles) # max velocity
         k = maxK
 
-        def canFinish(testK):
-            needHours = 0
+        def canEat(velocity):
+            time = 0
             for p in piles:
-                hours = p // testK
-                needHours += hours if p % testK == 0 else hours + 1
-            return needHours <= h
-
-        # binary search - for each k check if can finish
+                time += (p // velocity) + (1 if p % velocity else 0)
+            return time <= h
+        
         while minK <= maxK:
             midK = (minK + maxK) // 2
-            if canFinish(midK): # reduce
+
+            if canEat(midK):
                 k = midK
                 maxK = midK - 1
-            else: # try higher
+            else:
                 minK = midK + 1
         
         return k
-
-
