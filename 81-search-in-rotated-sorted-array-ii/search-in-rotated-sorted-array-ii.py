aa0eb7ca0a888cfac5ba1a3affa0f38ffbc96c7b
Tomás Ndlate <79701397+tomasndlate@users.noreply.github.com>
2025-07-17 08:15:30 +0100

    Time: 1 ms (12.04%) | Memory: 18.4 MB (32.01%) - LeetSync

--- 81-search-in-rotated-sorted-array-ii/search-in-rotated-sorted-array-ii.py
+++ 81-search-in-rotated-sorted-array-ii/search-in-rotated-sorted-array-ii.py
@@ -6,14 +6,14 @@ class Solution:
 
         while left <= right:
 
-            while left < right and (nums[left] == nums[left+1] or nums[left] == nums[right]):
+            while left < right and nums[left] == nums[left+1]:
                 left += 1
 
             while left < right and nums[right] == nums[right-1]:
                 right -= 1
 
-            #if left < right and nums[left] == nums[right]:
-                #left += 1
+            if left < right and nums[left] == nums[right]:
+                left += 1
 
             mid = (left + right) // 2
 
