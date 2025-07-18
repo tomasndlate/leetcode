e7fa8ab35254c66c7279186e6468c34baedb0966
Tomás Ndlate <79701397+tomasndlate@users.noreply.github.com>
2025-07-18 18:18:22 +0100

    Time: 4 ms (74.25%) | Memory: 18.2 MB (17.64%) - LeetSync

--- 47-permutations-ii/permutations-ii.py
+++ 47-permutations-ii/permutations-ii.py
@@ -11,12 +11,10 @@ class Solution:
             for i in range(start, len(nums)):
                 if nums[i] in seen:
                     continue
-
+                seen.add(nums[i])
                 nums[start], nums[i] = nums[i], nums[start]
                 backtrack(start + 1)
                 nums[start], nums[i] = nums[i], nums[start]
-                
-                seen.add(nums[i])
-
+        
         backtrack(0)
         return res
