10cc1eb4141b3205e110c9108b908d06e9ff0388
Tomás Ndlate <79701397+tomasndlate@users.noreply.github.com>
2025-07-18 17:46:05 +0100

    Time: 0 ms (100.00%) | Memory: 18.2 MB (15.73%) - LeetSync

--- 46-permutations/permutations.py
+++ 46-permutations/permutations.py
@@ -3,14 +3,14 @@ class Solution:
         res = []
 
         def backtrack(start):
-            if start >= len(nums):
+            if start == len(nums):
                 res.append(nums[:])
                 return
             
             for i in range(start, len(nums)):
                 nums[start], nums[i] = nums[i], nums[start]
-                backtrack(start+1)
+                backtrack(start + 1)
                 nums[start], nums[i] = nums[i], nums[start]
-
+        
         backtrack(0)
         return res
