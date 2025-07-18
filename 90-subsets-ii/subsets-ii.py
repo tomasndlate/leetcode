650ceba0a977812596c4ed7c6369f0d18fd58055
Tomás Ndlate <79701397+tomasndlate@users.noreply.github.com>
2025-07-18 17:53:02 +0100

    Time: 0 ms (100.00%) | Memory: 18.1 MB (15.06%) - LeetSync

--- 90-subsets-ii/subsets-ii.py
+++ 90-subsets-ii/subsets-ii.py
@@ -9,9 +9,8 @@ class Solution:
             for i in range(start, len(nums)):
                 if i > start and nums[i] == nums[i-1]:
                     continue
-
                 path.append(nums[i])
-                backtrack(i + 1, path)
+                backtrack(i+1, path)
                 path.pop()
         
         backtrack(0, [])
