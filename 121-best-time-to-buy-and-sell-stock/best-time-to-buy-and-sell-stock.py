d22908bf3651b3cde2624977e1b956d7047ba689
Tomás Ndlate <79701397+tomasndlate@users.noreply.github.com>
2025-07-19 14:08:09 +0100

    Time: 71 ms (71.61%) | Memory: 26.9 MB (77.45%) - LeetSync

--- 121-best-time-to-buy-and-sell-stock/best-time-to-buy-and-sell-stock.py
+++ 121-best-time-to-buy-and-sell-stock/best-time-to-buy-and-sell-stock.py
@@ -1,13 +1,15 @@
 class Solution:
     def maxProfit(self, prices: List[int]) -> int:
-        profit, i = 0, 0
-        for j in range(len(prices)):
-            # no profit in interval
-            if prices[i] >= prices[j]:
-                # update to sell lower
-                i = j 
-            # found a possible profit
-            else: 
-                profit = max(profit, prices[j] - prices[i])
-        return profit
-        
+        buy = float('inf')
+        maxProfit = 0
+
+        for price in prices:
+            
+            if price < buy:
+                buy = price
+
+            profit = price - buy
+            maxProfit = max(maxProfit, profit)
+
+
+        return maxProfit
