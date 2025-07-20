00f39cdb0d9cd2a0a0393086aaa7541be3a506d6
Tomás Ndlate <79701397+tomasndlate@users.noreply.github.com>
2025-07-20 12:08:06 +0100

    Time: 0 ms (100.00%) | Memory: 17.6 MB (86.11%) - LeetSync

--- 213-house-robber-ii/house-robber-ii.py
+++ 213-house-robber-ii/house-robber-ii.py
@@ -9,7 +9,6 @@ class Solution:
             house1, house2 = 0, 0
             for house in houses:
                 house1, house2 = house2, max(house1 + house, house2)
-            
             return max(house1, house2)
 
         return max(helper(nums[1:]), helper(nums[:n-1]))
