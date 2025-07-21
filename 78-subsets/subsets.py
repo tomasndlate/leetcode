e78440493add96bd7b94cd0fd77bf7059d648d0a
Tomás Ndlate <79701397+tomasndlate@users.noreply.github.com>
2025-07-21 20:28:49 +0100

    Time: 0 ms (100.00%) | Memory: 17.7 MB (90.82%) - LeetSync

--- 78-subsets/subsets.py
+++ 78-subsets/subsets.py
@@ -8,8 +8,8 @@ class Solution:
 
             for i in range(start, len(nums)):
                 path.append(nums[i])
-                backtrack(i+1, path)
+                backtrack(i + 1, path)
                 path.pop()
-        
+
         backtrack(0, [])
         return res
