07599c6460cd15ceb9e72826c140a1a634ce8498
Tomás Ndlate <79701397+tomasndlate@users.noreply.github.com>
2025-07-23 19:22:43 +0100

    Time: 3350 ms (76.15%) | Memory: 17.8 MB (65.03%) - LeetSync

--- 79-word-search/word-search.py
+++ 79-word-search/word-search.py
@@ -1,32 +1,34 @@
 class Solution:
     def exist(self, board: List[List[str]], word: str) -> bool:
-        rows, cols = len(board), len(board[0])
-        visited = [[False] * cols for _ in range(rows)]
-
-        def dfs(row, col, i):
-            if i == len(word): return True
-
-            nonlocal rows, cols
-            if ( row < 0 or row >= rows 
-                or col < 0 or col >= cols
-                or visited[row][col]
-                or board[row][col] != word[i]):
-                return False
-            
-            visited[row][col] = True
-            if (   dfs(row - 1, col, i+1) # up
-                or dfs(row + 1, col, i+1) # down
-                or dfs(row, col - 1, i+1) # left
-                or dfs(row, col + 1, i+1)): # right
-                return True
-            visited[row][col] = False
-
+        if not board or not word:
             return False
 
+        rows, cols = len(board), len(board[0])
+        directions = [(-1, 0), (1, 0), (0, -1), (0, 1)]
+        visiting = [ [False] * cols for _ in range(rows)]
+        def dfs(row, col, i):
+            if i == len(word) - 1:
+                return board[row][col] == word[i]
+            if board[row][col] != word[i]:
+                return False
 
-        for row in range(rows):
-            for col in range(cols):
-                if dfs(row, col, 0):
+            visiting[row][col] = True
+
+            for x, y in directions:
+                r = row + x
+                c = col + y
+                if (    0 <= r < rows
+                    and 0 <= c < cols
+                    and not visiting[r][c]
+                    and dfs(r, c, i+1)):
+                    return True
+
+            visiting[row][col] = False
+            return False
+
+        for r in range(rows):
+            for c in range(cols):
+                if dfs(r, c, 0):
                     return True
 
         return False
