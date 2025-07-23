9fdb87ffc509a40b6d30f85f441a2447b283688e
Tomás Ndlate <79701397+tomasndlate@users.noreply.github.com>
2025-07-23 19:41:54 +0100

    Time: 0 ms (100.00%) | Memory: 21.7 MB (57.56%) - LeetSync

--- 130-surrounded-regions/surrounded-regions.py
+++ 130-surrounded-regions/surrounded-regions.py
@@ -3,43 +3,35 @@ class Solution:
         """
         Do not return anything, modify board in-place instead.
         """
-        # CHECK NOT SURROUNDED - CHANGE TO TEMPORARY VALUE
-        # THEN REPLACE ALL VALUES
-
         rows, cols = len(board), len(board[0])
         directions = [(-1, 0), (1, 0), (0, -1), (0, 1)]
-        queue = collections.deque()
-        
-        # LOOP THROUGH BORDERS
-        ## horizontal
-        for c in range(cols):
-            if board[0][c] == "O":      queue.append((0, c))
-            if board[rows-1][c] == "O": queue.append((rows-1, c))
-        ## vertical
-        for r in range(rows):
-            if board[r][0] == "O":      queue.append((r, 0))
-            if board[r][cols-1] == "O": queue.append((r, cols-1))
-
-        while queue:
-            row, col = queue.popleft()
-            if board[row][col] == "V": continue
-
+        def dfs(row, col):
             board[row][col] = "V"
 
-            for rdir, cdir in directions:
-                r, c = row + rdir, col + cdir
-                if (   r < 0 or r >= rows 
-                    or c < 0 or c >= cols):
-                    continue # out of bounds
-                if board[r][c] == "O":
-                    queue.append((r, c))
-
-        # LOOP AND CHANGE (V->O), and (O->X)
+            for x, y in directions:
+                r = row + x
+                c = col + y
+                if (    0 <= r < rows
+                    and 0 <= c < cols
+                    and board[r][c] == "O"):
+                    dfs(r, c)
+        
+        # horizontal borders
+        for c in range(cols):
+            if board[0][c] == "O": # top
+                dfs(0, c)
+            if board[rows-1][c] == "O": # bottom
+                dfs(rows-1, c)
+        # vertical borders
+        for r in range(rows):
+            if board[r][0] == "O": # left
+                dfs(r, 0)
+            if board[r][cols-1] == "O": # right
+                dfs(r, cols-1)
+        
         for r in range(rows):
             for c in range(cols):
-                if board[r][c] == "O": # surrounded
+                if board[r][c] == "O":
                     board[r][c] = "X"
-                if board[r][c] == "V": # not surrounded
+                elif board[r][c] == "V":
                     board[r][c] = "O"
-        
-        return board
