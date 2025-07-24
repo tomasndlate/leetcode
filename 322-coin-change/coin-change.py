f40df1c52e250ff0d4641b100fe5d5843805b4ed
Tomás Ndlate <79701397+tomasndlate@users.noreply.github.com>
2025-07-24 12:27:32 +0100

    Time: 994 ms (27.22%) | Memory: 47.5 MB (5.71%) - LeetSync

--- 322-coin-change/coin-change.py
+++ 322-coin-change/coin-change.py
@@ -1,23 +1,22 @@
 class Solution:
     def coinChange(self, coins: List[int], amount: int) -> int:
+        coins.sort(reverse=True)
         memo = {}
-        coins.sort()
 
         def dp(remaining):
-            if remaining in memo:
-                return memo[remaining]
-            if remaining == 0:
-                return 0
-            if remaining < 0:
-                return -1
+            if remaining in memo: return memo[remaining]
+            if remaining < 0: return -1
+            if remaining == 0: return 0
+            
+            usedCoins = amount + 1
+            for coin in coins:
+                otherCoins = dp(remaining - coin)
+                if otherCoins == -1: # impossible to reach
+                    continue
+                usedCoins = min(usedCoins, 1 + otherCoins)
 
-            min_coins = float('inf')
-            for coin in reversed(coins):
-                res = dp(remaining - coin)
-                if res >= 0:
-                    min_coins = min(min_coins, 1 + res)
-                    
-            memo[remaining] = min_coins if min_coins < float('inf') else -1
+            memo[remaining] = usedCoins if usedCoins <= amount else -1
             return memo[remaining]
-
+        
         return dp(amount)
+                
