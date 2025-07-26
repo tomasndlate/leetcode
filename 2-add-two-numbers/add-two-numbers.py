fd23655526f9abbe6507d96b2382376ec7da1c2a
Tomás Ndlate <79701397+tomasndlate@users.noreply.github.com>
2025-07-26 15:59:25 +0100

    Time: 0 ms (100.00%) | Memory: 18.1 MB (14.76%) - LeetSync

--- 2-add-two-numbers/add-two-numbers.py
+++ 2-add-two-numbers/add-two-numbers.py
@@ -10,19 +10,18 @@ class Solution:
         cur = dummy
 
         rest = 0
-
         while l1 or l2 or rest:
-            v1 = l1.val if l1 else 0
-            v2 = l2.val if l2 else 0
-            num = v1 + v2 + rest
+            num1 = l1.val if l1 else 0
+            num2 = l2.val if l2 else 0
+            num = num1 + num2 + rest
 
-            rest = num // 10
             digit = num % 10
+            rest = num // 10
 
             cur.next = ListNode(val=digit)
             cur = cur.next
-            
+
             if l1: l1 = l1.next
             if l2: l2 = l2.next
-        
-        return dummy.next
+
+        return dummy.next 
