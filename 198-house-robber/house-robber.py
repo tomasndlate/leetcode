5a1e4eb05805f48504166bc3aa7d58f7536a80a3
Tomás Ndlate <79701397+tomasndlate@users.noreply.github.com>
2025-07-27 16:24:58 +0100

    Time: 0 ms (100.00%) | Memory: 17.7 MB (82.88%) - LeetSync

--- 198-house-robber/house-robber.py
+++ 198-house-robber/house-robber.py
@@ -1,8 +1,13 @@
 class Solution:
     def rob(self, nums: List[int]) -> int:
+        house1 = 0
+        house2 = 0
+
+        for house in nums:
+
+            robHouse = house + house1
+            skipHouse = house2
+
+            house1, house2 = house2, max(robHouse, skipHouse)
         
-        house1, house2 = 0, 0
-        for num in nums:
-            house1, house2 = house2, max(house1 + num, house2)
-        
-        return max(house1, house2)
+        return house2
