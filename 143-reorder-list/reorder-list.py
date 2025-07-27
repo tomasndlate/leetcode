6f14b37c46449b4c2c35ebc528b2226b5bc5f3c3
Tomás Ndlate <79701397+tomasndlate@users.noreply.github.com>
2025-07-27 12:27:36 +0100

    Time: 3 ms (46.17%) | Memory: 23.4 MB (38.48%) - LeetSync

--- 143-reorder-list/reorder-list.py
+++ 143-reorder-list/reorder-list.py
@@ -8,32 +8,38 @@ class Solution:
         """
         Do not return anything, modify head in-place instead.
         """
-        # LINKED LIST
-        dummy = ListNode(next=head)
-
-        # Find middle
-        slow, fast = dummy, dummy
+        if not head:
+            return None
+        
+        # Find mid node
+        slow = fast = ListNode(next=head)
         while fast and fast.next:
             slow = slow.next
             fast = fast.next.next
-
-        # Revert pointer to point back (slow is the last of 1st half)
-        cur = slow.next
-        prev = None
-        slow.next = None
-        while cur:
-            temp = cur.next
-            cur.next = prev
-            prev = cur
-            cur = temp
         
-        # Merge both lists
-        l1, l2 = head, prev # prev contains last value 2nd list
-        while l1 and l2:
-            temp1, temp2 = l1.next, l2.next
-            l1.next = l2
-            l2.next = temp1
-            l1, l2 = temp1, temp2
+        # Unlink both halfs
+        second = slow.next
+        slow.next = None
+
+        # Revert 2nd half
+        prev = None
+        while second:
+            temp = second.next
+            second.next = prev
+            prev = second
+            second = temp
+        
+        # Merge both halfs
+        first = head
+        second = prev
+        while first and second:
+            temp1 = first.next
+            temp2 = second.next
+
+            first.next = second
+            second.next = temp1
+
+            first = temp1
+            second = temp2
         
         return head
-        
