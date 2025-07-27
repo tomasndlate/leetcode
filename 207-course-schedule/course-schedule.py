d39a1a9cb38acad738663c3635f33a7f63d3d0ca
Tomás Ndlate <79701397+tomasndlate@users.noreply.github.com>
2025-07-27 13:03:11 +0100

    Time: 10 ms (15.52%) | Memory: 19.3 MB (52.11%) - LeetSync

--- 207-course-schedule/course-schedule.py
+++ 207-course-schedule/course-schedule.py
@@ -1,31 +1,28 @@
+from collections import defaultdict
 class Solution:
     def canFinish(self, numCourses: int, prerequisites: List[List[int]]) -> bool:
-        preMap = {} # course: [pre]
+        graph = defaultdict(list)
         for course, pre in prerequisites:
-            if course not in preMap:
-                preMap[course] = []
-            preMap[course].append(pre)
+            graph[course].append(pre)
         
-        visited = set()
         visiting = set()
-        def detectCycle(course):
+        visited = set()
+        def isCycle(course): # dfs
             if course in visiting:
                 return True
             if course in visited:
                 return False
             
             visiting.add(course)
-            for pre in preMap.get(course, []):
-                if detectCycle(pre):
-                    return True
-
+            if course in graph:
+                for pre in graph[course]:
+                    if isCycle(pre):
+                        return True
             visiting.remove(course)
             visited.add(course)
-            return False
         
-        # loop through courses, and check cycles
-        for c in range(numCourses):
-            if detectCycle(c):
+        for course in graph.keys():
+            if course not in visited and isCycle(course):
                 return False
         
         return True
