56c6f90386938e75fab9f0f386d0cc1db99681a0
Tomás Ndlate <79701397+tomasndlate@users.noreply.github.com>
2025-08-13 19:57:48 +0100

    Time: 3 ms (96.86%) | Memory: 18.8 MB (36.05%) - LeetSync

--- 45-jump-game-ii/jump-game-ii.py
+++ 45-jump-game-ii/jump-game-ii.py
@@ -1,14 +1,19 @@
+from collections import deque
 class Solution:
     def jump(self, nums: List[int]) -> int:
-        jumps = 0
-        l = r = 0
-
-        while r < len(nums) - 1: # didn't reach final
-            jumps += 1
-            farthest = 0
-            for i in range(l, r + 1):
-                farthest = max(farthest, i + nums[i])
-            l = r + 1
-            r = farthest
         
-        return jumps
+        jumps = 0
+        left, right = 0, 0
+
+        while left <= right:
+            if right >= len(nums) - 1:
+                return jumps
+
+            farthest = left
+            for i in range(left, right + 1):
+                farthest = max(farthest, i + nums[i])
+
+            left, right = right + 1, farthest
+            jumps += 1
+        
+        return 0
