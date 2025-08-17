f7ef06d33765d597632d6a8c4e663618c14324e1
Tomás Ndlate <79701397+tomasndlate@users.noreply.github.com>
2025-08-17 15:38:48 +0100

    Time: 7 ms (23.66%) | Memory: 17.9 MB (64.21%) - LeetSync

--- 139-word-break/word-break.py
+++ 139-word-break/word-break.py
@@ -1,24 +1,21 @@
 class Solution:
     def wordBreak(self, s: str, wordDict: List[str]) -> bool:
-        
         words = set(wordDict)
-        memo = {}
+        cache = {}
 
         def dp(start):
-            if start in memo:
-                return memo[start]
-
             if start == len(s):
                 return True
-
-            # possibilities starting at start
-            for i in range(start, len(s)):
-                if s[start:i+1] in words:
-                    if dp(i+1):
-                        memo[start] = True
-                        return memo[start]
+            if start in cache:
+                return cache[start]
             
-            memo[start] = False
-            return memo[start]
+            for i in range(start, len(s)):
+                # if word in dict and other words as well
+                if s[start:i+1] in words and dp(i+1):
+                    cache[start] = True
+                    return cache[start]
+            
+            cache[start] = False
+            return cache[start]
         
         return dp(0)
