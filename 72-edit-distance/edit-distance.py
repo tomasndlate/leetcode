2a9b2bf7e1db5e75c165b8afd52bcf1e1f5b01f6
Tomás Ndlate <79701397+tomasndlate@users.noreply.github.com>
2025-08-18 08:29:03 +0100

    Time: 100 ms (6.79%) | Memory: 19.6 MB (81.23%) - LeetSync

--- 72-edit-distance/edit-distance.py
+++ 72-edit-distance/edit-distance.py
@@ -3,27 +3,25 @@ class Solution:
         cache = {}
 
         def dp(w1, w2):
-            # Equal words
+            if (w1, w2) in cache:
+                return cache[(w1, w2)]
+            # Both words equal
             if w1 == len(word1) and w2 == len(word2):
                 return 0
-            # Transformations left in the other word
+            # One word done
             if w1 == len(word1): return len(word2) - w2
             if w2 == len(word2): return len(word1) - w1
 
-            if (w1, w2) in cache:
-                return cache[(w1, w2)]
-
-            # No transformation required, same letter, no cost
+            # Equal letters, no transformation need (no cost)
             if word1[w1] == word2[w2]:
-                cache[(w1, w2)] = dp(w1 + 1, w2 + 1)
-                return cache[(w1, w2)]
+                return dp(w1 + 1, w2 + 1)
 
-            # Transformation required, cost 1
-            insert = dp(w1, w2 + 1)
-            delete = dp(w1 + 1, w2)
-            replace = dp(w1 + 1, w2 + 1)
+            # Different letters, transformation need (cost)
+            insert = 1 + dp(w1, w2 + 1)
+            delete = 1 + dp(w1 + 1, w2)
+            replace = 1 + dp(w1 + 1, w2 + 1)
 
-            cache[(w1, w2)] = 1 + min(insert, delete, replace)
+            cache[(w1, w2)] = min(insert, delete, replace)
             return cache[(w1, w2)]
-
+        
         return dp(0, 0)
