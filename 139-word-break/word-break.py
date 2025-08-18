b8058d536b401c373bb5cf139e83369f379abd3a
Tomás Ndlate <79701397+tomasndlate@users.noreply.github.com>
2025-08-18 18:13:17 +0100

    Time: 3 ms (69.63%) | Memory: 18.1 MB (12.92%) - LeetSync

--- 139-word-break/word-break.py
+++ 139-word-break/word-break.py
@@ -8,14 +8,13 @@ class Solution:
                 return True
             if start in cache:
                 return cache[start]
-            
+
             for i in range(start, len(s)):
-                # if word in dict and other words as well
                 if s[start:i+1] in words and dp(i+1):
                     cache[start] = True
-                    return cache[start]
+                    return True
             
             cache[start] = False
-            return cache[start]
-        
+            return False
+
         return dp(0)
