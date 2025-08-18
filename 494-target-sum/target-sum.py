b37cd022c9ed0f801a5ab11fc434edc6eb871aad
Tomás Ndlate <79701397+tomasndlate@users.noreply.github.com>
2025-08-18 08:59:10 +0100

    Time: 218 ms (12.98%) | Memory: 38.4 MB (26.64%) - LeetSync

--- 494-target-sum/target-sum.py
+++ 494-target-sum/target-sum.py
@@ -1,14 +1,13 @@
 class Solution:
     def findTargetSumWays(self, nums: List[int], target: int) -> int:
         cache = {}
-        
+
         def dp(i, total):
             if i == len(nums):
                 return 1 if total == target else 0
             if (i, total) in cache:
                 return cache[(i, total)]
-            
-            # Possible Ways
+
             negative = dp(i + 1, total - nums[i])
             positive = dp(i + 1, total + nums[i])
 
@@ -16,6 +15,3 @@ class Solution:
             return cache[(i, total)]
         
         return dp(0, 0)
-
-
-            
