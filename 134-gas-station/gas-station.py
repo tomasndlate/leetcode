9773e2b51e80d8d4f2de6c8ac90cc9a94b56578e
Tomás Ndlate <79701397+tomasndlate@users.noreply.github.com>
2025-08-19 08:33:37 +0100

    Time: 34 ms (33.23%) | Memory: 23.2 MB (96.24%) - LeetSync

--- 134-gas-station/gas-station.py
+++ 134-gas-station/gas-station.py
@@ -1,18 +1,16 @@
 class Solution:
     def canCompleteCircuit(self, gas: List[int], cost: List[int]) -> int:
-        diff = [g - c for g, c in zip(gas, cost)]
-        
-        if sum(diff) < 0: # impossible
+        # Impossible, at some point no fuel
+        if sum(g - c for g, c in zip(gas, cost)) < 0:
             return -1
-        
-        accumulated = 0
+
+        fuel = 0
         index = 0
 
-        for i, fuel in enumerate(diff):
-            accumulated += fuel
-            if accumulated < 0: # don't reach next
-                accumulated = 0
+        for i in range(len(gas)):
+            fuel += gas[i] - cost[i]
+            if fuel < 0:
+                fuel = 0
                 index = i + 1
         
         return index
-
